35329b7ea1dbf114af64c2ef7820532f315ee256
nataphilips <fnd.natalia@gmail.com>
2019-11-05 12:27:21 +0200

    Database setup

---
+++ src/database/sql_build.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const dbConnection = require("./db_connection.js");
+
+const sqlPath = path.join(__dirname, "db_build.sql");
+const sql = fs.readFileSync(sqlPath).toString();
+
+dbConnection.query(sql, (err, result) => {
+  if (err) {
+    console.log(err, "error");
+  } else {
+    console.log("database created");
+    dbConnection.end(() => {
+      console.log("connection closed");
+    });
+  }
+});
