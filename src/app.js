fd8cadcb6c86909688ad32bdf7683f18f169a319
nataphilips <fnd.natalia@gmail.com>
2019-11-06 14:14:37 +0200

    Log in

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const routes = require("./routes/index.js");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 
